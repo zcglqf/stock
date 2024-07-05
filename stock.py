d809ccab4bc9e74ebf7673243f972b035533a9f3
Chenguang Zhao <chenguang@imaginostics.com>
2024-07-05 09:17:54 -0400

    Read and show employment data looks ok.

--- stock.py
+++ stock.py
@@ -3,20 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import mplcursors
 import matplotlib.font_manager as fm
-import matplotlib.cm as cm
-import matplotlib as mpl
-from matplotlib.colors import LinearSegmentedColormap, ListedColormap
 import numpy as np
 
-# Try to create a colormap that can mostly seperate the colors
-top = mpl.colormaps['Oranges_r'].resampled(128)
-middle = mpl.colormaps['Greens'].resampled(128)
-bottom = mpl.colormaps['Blues'].resampled(128)
-
-newcolors = np.vstack((top(np.linspace(0, 1, 128)),
-                       middle(np.linspace(0, 1, 128)),
-                       bottom(np.linspace(0, 1, 128))))
-newcmp = ListedColormap(newcolors, name='newRGB')
 
 
 
@@ -32,128 +20,192 @@ BLS_API_URL = "https://api.bls.gov/publicAPI/v2/timeseries/data/"
 API_KEY = "8da8111c50c846ab971494707ae44d4c"
 headers = {'Content-type': 'application/json'}
 
-# Example payload for unemployment rate (Series ID: 'LNS14000000')
-employmentPicksID = ['LNS11000000',                
-                     'LNS12000000',             
-                     'LNS13000000',               
-                     'LNS14000000',           
-                     'CES0000000001',                          
-                     'CES0500000002',                                       
-                     'CES0500000007', 
-                     'CES0500000003',
-                     'CES0500000008']
-
-employmentPicksName = ['Civilian Labor Force (K)', 
-                       'Civilian Employment (K)', 
-                       'Civilian Unemployment (K)', 
-                       'Unemployment Rate (%)', #
-                       'Total Nonfarm Employment (K)', #
-                       'Total Private Average Weekly Hours of All Employee (Hours)', 
-                       'Total Private Average Weekly Hours of Prod. and Nonsup. Employees (Hours)',  #
-                       'Total Private Average Hourly Earnings of All Employees (USD)',
-                       'Total Private Average Hourly Earnings of Prod. and Nonsup. Employees (Hours)'] #
-
-employmentPicksNameChinese = ['全美劳动人口（千人）', 
-                              '全美在业人口（千人）',
-                              '全美失业人口（千人）',
-                              '失业率（％）',
-                              '非农就业人口（千人）',
-                              '私有部门平均每周工作时间（小时）',
-                              '私有部门生产非管理人员平均每周工作时间（小时）',
-                              '私有部门平均时薪（美元）',
-                              '私有部门生产非管理人员时薪（美元）']
-
-employmentPicksUnit = ['K', 'K', 'K', 'Percentage(%)', 'Head Counts', 'Hours', 'Hours', 'USD', 'USD']
-
-employmentPicksColor = newcmp.resampled(len(employmentPicksID)) # ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w', 'w']
+employmentPicksColor = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w', 'w'] # newcmp.resampled(len(employmentPicksID)) # 
 
 employmentPicksMarker = ['.', 's', 'v', 'p', '*', '+', 'x', 'D', '^', '<', '>', 'k', 'w']
 
-employmentPicksLineWidth = [0.5, 
-                            0.5, 
-                            0.5, 
-                            3, #
-                            3, #
-                            0.5, 
-                            3,  #
-                            0.5,
-                            3] # 
 
-employmentPicksShareSameAxisWith = [-1, 0, 0, -1, 0, -1, 5, -1, 8]   # y-axis reuse table
-employmentPicksYLim = [[150000, 200000],
-                       [150000, 200000],
-                       [150000, 200000],
-                       [0, 0.1],
-                       [150000, 200000],
-                       [20, 40],
-                       [20, 40],
-                       [20, 40],
-                       [20, 40]
-                       ]
+# We organize certain data series into one group, they are revalent and can be plotted into on figure. 
+employmentPicksGroups = [ 
+    # Group 1
+    [
+        {'ID': 'LNS11000000', 
+        'EnglishName': 'Civilian Labor Force (K)',
+        'ChineseName': '全美劳动人口（千人）',
+        'Unit': 'K',
+        'Color': employmentPicksColor[0],
+        'Marker': employmentPicksMarker[0],
+        'XLim': None,
+        'YLim': [150000, 200000],
+        'YAxisPos': 'left',
+        'Curve': None},
 
-dataUnemploymentRate = {
-    "seriesid": employmentPicksID,
-    "startyear": "2023",
-    "endyear": "2024",
-    "registrationkey": API_KEY
-}
+        {'ID': 'LNS12000000', 
+        'EnglishName': 'Civilian Employment (K)',
+        'ChineseName': '全美在业人口（千人）',
+        'Unit': 'K',
+        'Color': employmentPicksColor[1],
+        'Marker': employmentPicksMarker[1],
+        'XLim': None,
+        'YLim': [150000, 200000],
+        'YAxisPos': 'left',
+        'Curve': None},      
 
-response = requests.post(BLS_API_URL, json=dataUnemploymentRate, headers=headers)
-json_data = response.json()
+        {'ID': 'CES0000000001', 
+        'EnglishName': 'Total Nonfarm Employment (K)',
+        'ChineseName': '非农就业人口（千人）',
+        'Unit': 'K',
+        'Color': employmentPicksColor[4],
+        'Marker': employmentPicksMarker[4],
+        'XLim': None,
+        'YLim': [150000, 200000],
+        'YAxisPos': 'left',
+        'Curve': None}
+    ],
 
-#
-# Extract data and plot
-axes = [None] * len(json_data['Results']['series'])
-curves = [None] * len(json_data['Results']['series'])
-fig, ax1 = plt.subplots(figsize=(12, 8))
+    # Group 2
+    [
+        {'ID': 'LNS13000000', 
+        'EnglishName': 'Civilian Unemployment (K)',
+        'ChineseName': '全美失业人口（千人）',
+        'Unit': 'K',
+        'Color': employmentPicksColor[2],
+        'Marker': employmentPicksMarker[2],
+        'XLim': None,
+        'YLim': [150000, 200000],
+        'YAxisPos': 'left',
+        'Curve': None},                
 
-series_data = json_data['Results']['series']
-for seriesID in range(len(series_data)):
-    df = pd.DataFrame(series_data[seriesID]['data'])
+        {'ID': 'LNS14000000', 
+        'EnglishName': 'Unemployment Rate (%)',
+        'ChineseName': '失业率（％）',
+        'Unit': '%',
+        'Color': employmentPicksColor[3],
+        'Marker': employmentPicksMarker[3],
+        'XLim': None,
+        'YLim': [3, 10],
+        'YAxisPos': 'right',
+        'Curve': None},  
+    ],
 
-    # Convert date format and sort by date
-    df['year'] = df['year'].astype(int)
-    df['period'] = df['period'].apply(lambda x: x.replace('M', ''))
-    df['month'] = df['period'].astype(int)
-    df['date'] = pd.to_datetime(df[['year', 'month']].assign(day=1))
-    df = df.sort_values('date')
+    # Group 3
+    [
+        {'ID': 'CES0500000002', 
+         'EnglishName': 'Total Private Average Weekly Hours of All Employee (Hours)',
+         'ChineseName': '私有部门平均每周工作时间（小时）',
+         'Unit': 'Hours',
+         'Color': employmentPicksColor[0],
+         'Marker': employmentPicksMarker[0],
+         'XLim': None,
+         'YLim': [20, 40],
+         'YAxisPos': 'left',
+         'Curve': None},
 
-    # Convert value to float and set date as index
-    df['value'] = df['value'].astype(float)
-    df.set_index('date', inplace=True)
+        {'ID': 'CES0500000007', 
+         'EnglishName': 'Total Private Average Weekly Hours of Prod. and Nonsup. Employees (Hours)',
+         'ChineseName': '私有部门生产非管理人员平均每周工作时间（小时）',
+         'Unit': 'Hours',
+         'Color': employmentPicksColor[1],
+         'Marker': employmentPicksMarker[1],
+         'XLim': None,
+         'YLim': [20, 40],
+         'YAxisPos': 'left',
+         'Curve': None},
 
-    # Plot
-    axes[seriesID] = ax1 if seriesID == 0 else ax1.twinx()
-    curves[seriesID], = axes[seriesID].plot(df.index, df['value'], marker=employmentPicksMarker[seriesID], linestyle='-', color=employmentPicksColor(seriesID), label=employmentPicksName[seriesID], linewidth = employmentPicksLineWidth[seriesID])
-    axes[seriesID].set_xlabel('Date')
-    axes[seriesID].set_ylabel(employmentPicksUnit[seriesID], color=employmentPicksColor(seriesID))
-    axes[seriesID].tick_params(axis='y', labelcolor=employmentPicksColor(seriesID))
+        {'ID': 'CES0500000003', 
+         'EnglishName': 'Total Private Average Hourly Earnings of All Employees (USD)',
+         'ChineseName': '私有部门平均时薪（美元）',
+         'Unit': 'USD',
+         'Color': employmentPicksColor[2],
+         'Marker': employmentPicksMarker[2],
+         'XLim': None,
+         'YLim': [20, 40],
+         'YAxisPos': 'right',
+         'Curve': None},
+
+        {'ID': 'CES0500000008', 
+         'EnglishName': 'Total Private Average Hourly Earnings of Prod. and Nonsup. Employees (USD)',
+         'ChineseName': '私有部门生产非管理人员时薪（美元）',
+         'Unit': 'USD',
+         'Color': employmentPicksColor[3],
+         'Marker': employmentPicksMarker[3],
+         'XLim': None,
+         'YLim': [20, 40],
+         'YAxisPos': 'right',
+         'Curve': None}
+    ]
+]
+
+totalNrDataSeries = sum([len(group) for group in employmentPicksGroups])
+# curves = [None] * totalNrDataSeries
+fig, axes = plt.subplots(len(employmentPicksGroups), 1, figsize=(12, 8))
+twinaxes = [ax.twinx() for ax in axes] 
+
+for groupidx, group in enumerate(employmentPicksGroups):
+    # Retrive data series IDs from current group
+    seriesIDs = [series['ID'] for series in group]
+
+    dataRequestInfo = {
+        "seriesid": seriesIDs,
+        "startyear": "2023",
+        "endyear": "2024",
+        "registrationkey": API_KEY
+    }
+
+    response = requests.post(BLS_API_URL, json=dataRequestInfo, headers=headers)
+    json_data = response.json()
+    series_data = json_data['Results']['series']
     
-    (N, r) = divmod(seriesID, 2) 
-    labelShift = 60 * N if N > 0 else 0
-    labelLeftRight = 'left' if r == 0 else 'right'
-    # if seriesID > 0:
-    #axes[seriesID].spines['right'].set_position(('outward', 60 * (seriesID-1)))
-    axes[seriesID].yaxis.set_label_position(labelLeftRight)
-    axes[seriesID].spines[labelLeftRight].set_position(('outward', labelShift))
-    axes[seriesID].yaxis.tick_left() if r == 0 else axes[seriesID].yaxis.tick_right()
-    #handles, labels = axes[seriesID].get_legend_handles_labels()
-    #axes[seriesID].legend(loc='upper right', bbox_to_anchor=(1,1)) if handles else None
-    axes[seriesID].set_ylim(employmentPicksYLim[seriesID])
-    # Resuse y-axis lim and tick of another data
-"""     if employmentPicksShareSameAxisWith[seriesID] != -1 :
-         axes[seriesID].set_ylim(axes[employmentPicksShareSameAxisWith[seriesID]].get_ylim())
-         #axes[seriesID].set_yticks(axes[employmentPicksShareSameAxisWith[seriesID]].get_yticks())
-         axes[seriesID].spines[labelLeftRight].set_color(employmentPicksColor(seriesID))
-         #axes[seriesID].yaxis.set_ticks_position('none') """
 
+    # load and show data series in current group
+    ylimleftmin, ylimleftmax = float('inf'), -float('inf')
+    ylimrightmin, ylimrightmax = float('inf'), -float('inf')
+    for seriesID in range(len(series_data)):
+        df = pd.DataFrame(series_data[seriesID]['data'])
+        # Convert date format and sort by date
+        df['year'] = df['year'].astype(int)
+        df['period'] = df['period'].apply(lambda x: x.replace('M', ''))
+        df['month'] = df['period'].astype(int)
+        df['date'] = pd.to_datetime(df[['year', 'month']].assign(day=1))
+        df = df.sort_values('date')
+        # Convert value to float and set date as index
+        df['value'] = df['value'].astype(float)
+        df.set_index('date', inplace=True)
+
+        # Plot
+        if group[seriesID]['YAxisPos'] == 'left':
+            group[seriesID]['Curve'], = axes[groupidx].plot(df.index, df['value'], marker=group[seriesID]['Marker'], linestyle='-', color=group[seriesID]['Color'], label=group[seriesID]['EnglishName'])
+            axes[groupidx].set_xlabel('Date')
+            axes[groupidx].set_ylabel(group[seriesID]['Unit'])
+            axes[groupidx].yaxis.set_label_position(group[seriesID]['YAxisPos'])
+            axes[groupidx].yaxis.tick_left()
+            #axes[groupidx].set_ylim(group[seriesID]['YLim'])   
+            ylimleftmin = np.min(df['value']) if ylimleftmin > np.min(df['value']) else ylimleftmin
+            ylimleftmax = np.max(df['value']) if ylimleftmax < np.min(df['value']) else ylimleftmax
+        else:
+            group[seriesID]['Curve'], = twinaxes[groupidx].plot(df.index, df['value'], marker=group[seriesID]['Marker'], linestyle='-', color=group[seriesID]['Color'], label=group[seriesID]['EnglishName'])
+            twinaxes[groupidx].set_xlabel('Date')
+            twinaxes[groupidx].set_ylabel(group[seriesID]['Unit'])
+            twinaxes[groupidx].yaxis.set_label_position(group[seriesID]['YAxisPos'])
+            twinaxes[groupidx].yaxis.tick_right()
+            # twinaxes[groupidx].set_ylim(group[seriesID]['YLim'])    
+            ylimrightmin = np.min(df['value']) if ylimrightmin > np.min(df['value']) else ylimrightmin
+            ylimrightmax = np.max(df['value']) if ylimrightmax < np.min(df['value']) else ylimrightmax
+        
+
+    # Combine Legend Handles and Labels from Both Axes, legend text comes from the label property of each plot. 
+    lines_1, labels_1 = axes[groupidx].get_legend_handles_labels()
+    lines_2, labels_2 = twinaxes[groupidx].get_legend_handles_labels()
+    lines = lines_1 + lines_2
+    labels = labels_1 + labels_2
+    axes[groupidx].legend(lines, labels, loc='upper left')
+
+    curves = [series['Curve'] for series in group]
+    mplcursors.cursor(curves, hover=True) # enable cursor
 
 # Add a title and show the plot
-plt.title('Employment Data')
-fig.legend(employmentPicksNameChinese, loc='upper center',  bbox_to_anchor=(0.5, 0.95), fontsize=6)
+fig.suptitle('Employment Data')
+
 fig.tight_layout()  # Adjust layout to prevent overlap
-mplcursors.cursor(curves, hover=True) # enable cursor
 
 plt.show()
-
-
